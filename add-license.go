d2191a37bb95bb8c9eaf0c20d0a2d5e49723cfe5
Kentaro Sakai <kentasukentas@gmail.com>
2026-04-06 01:21:31 +0000

    added some feature: extension fileter

--- add-license.go
+++ add-license.go
@@ -5,56 +5,85 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 )
 
-// 0. get a file list and start iterate over the list.
-// 1. load a file and buffer contents.
-// 2. add a license text at the top of the file.
-// 3. append rest of the file contents.
-// 4. flush a file and exit.
-
-// possible errors
-// - can't get a file list.
-// - can't open files
-// - permission error: can't write to the file
-
-const (
-	MaxBufferSize int64  = 10 * 1024 * 1024 // 10MB
-	LicenseText   string = "/* \n * Copyright (c) 2026 Your Company. All rights reserved.\n * This file is subject to the terms and conditions defined in LICENSE.txt\n */\n\n"
+const MaxBufferSize int64 = 10 * 1024 * 1024 // 10MB
+var (
+	LicenseText string = ""
+	err         error
 )
 
 func main() {
-	var rootPath *string
-	rootPath = flag.String("path", "", "The root directory of the Java project")
+	var (
+		rootPath    *string
+		licensePath *string
+		ext         *string
+	)
+	rootPath    = flag.String("p", "path", "The root directory of the Java project")
+	licensePath = flag.String("l", "license", "The file location of the license text.")
+	ext         = flag.String("e", "extention", "define what file types to apply append.")
+
 	flag.Parse()
 
 	if *rootPath == "" {
 		fmt.Println("Error: Please provide a project path using -path")
 		os.Exit(1)
 	}
+	if *licensePath == "" {
+		fmt.Println("Error: Please provide a license path using -license")
+		os.Exit(1)
+	}
+	if *ext == "" {
+		fmt.Println("Error: Please provide which file type to modify.")
+		os.Exit(1)
+	}
+
+	var extSet *map[string]bool
+	extSet, err = createExtensionMap(*ext)
+	if err != nil {
+		log.Fatalf("Failed to load extension set from cli arguments.%v\n", err)
+		os.Exit(1)
+	}
+
+	var licenseFile *os.File
+	licenseFile, err = os.Open(*licensePath)
+	if err != nil {
+		log.Fatalf("Failed to open a license file. %v\n", err)
+		os.Exit(1)
+	}
+	defer licenseFile.Close()
+
+	LicenseText, err = loadLicenseText(licenseFile)
+	if err != nil {
+		log.Fatalf("Failed to get a license text from license file. %v\n", err)
+		os.Exit(1)
+	}
 
 	var (
 		info os.FileInfo
-		err error
-	) 
+		err  error
+	)
 	info, err = os.Stat(*rootPath)
 
 	if os.IsNotExist(err) {
 		fmt.Printf("Error: The path '%s' does not exist.\n", *rootPath)
 		os.Exit(1)
 	}
-	
-	var javaFiles []string
-	javaFiles, err = findJavaFiles(*rootPath)
+
+	var files []string
+	files, err = findExtension(*rootPath, extSet)
 	if err != nil {
 		fmt.Printf("Error scanning directory: %v\n", err)
 		os.Exit(1)
 	}
 
 	var path string
-	for _, path = range javaFiles {
+	for _, path = range files {
 		err = prependLicense(path, &info)
 		if err != nil {
 			fmt.Printf("Failed to process [%s]: %v\n", path, err)
@@ -64,14 +93,48 @@ func main() {
 	}
 }
 
-func findJavaFiles(root string) ([]string, error) {
+func createExtensionMap(str string) (*map[string]bool, error) {
+	if str == "" {
+		return nil, err
+	}
+	var (
+		extSet  map[string]bool = make(map[string]bool)
+		splitedString []string        = strings.Split(str, ",")
+	)
+	for i := range splitedString {
+		extSet["." + splitedString[i]] = true
+	}
+	return &extSet, nil
+}
+
+func loadLicenseText(file *os.File) (string, error) {
+	var (
+		reader   *bufio.Reader = bufio.NewReader(file)
+		contents []byte
+	)
+	for {
+		var buffer []byte
+		buffer, err = reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to get a license text from a flie. %v\n", err)
+			return "", err
+		}
+		contents = slices.Concat(contents, buffer)
+	}
+	return string(contents), nil
+}
+
+func findExtension(root string, extSet *map[string]bool) ([]string, error) {
 	var files []string
 	var err error
 	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() && filepath.Ext(path) == ".java" {
+		if !info.IsDir() && (*extSet)[filepath.Ext(path)] == true {
 			files = append(files, path)
 		}
 		return nil
@@ -82,8 +145,8 @@ func findJavaFiles(root string) ([]string, error) {
 func prependLicense(filePath string, originalMode *os.FileInfo) error {
 	var (
 		file_ori *os.File
-		err error
-		info os.FileInfo
+		err      error
+		info     os.FileInfo
 		// originalMode os.FileMode
 	)
 	// Get original info first to check permissions and mode
@@ -120,7 +183,7 @@ func prependLicense(filePath string, originalMode *os.FileInfo) error {
 		}
 		tempFile.Seek(0, 0)
 		originalContentReader = tempFile
-		
+
 		// Large file writing
 		return rewriteFileFromReader(filePath, LicenseText, originalContentReader, (*originalMode).Mode())
 	} else {
